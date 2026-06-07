a82c6d2845746e097a6ee16de13e0b1235ab7252
kingbenny143 <anandanishraj@gmail.com>
2026-06-07 21:17:19 +0530

    dashboard layout

--- frontend/src/layouts/DashboardLayout.jsx
+++ frontend/src/layouts/DashboardLayout.jsx
@@ -4,13 +4,13 @@ import Topbar from "../components/layout/Topbar";
 
 const DashboardLayout = () => {
   return (
-    <div className="flex min-h-screen">
+    <div className="flex h-screen">
       <Sidebar />
 
-      <div className="flex-1">
+      <div className="flex-1 flex flex-col">
         <Topbar />
 
-        <main className="p-6">
+        <main className="flex-1 overflow-y-auto p-6">
           <Outlet />
         </main>
       </div>
